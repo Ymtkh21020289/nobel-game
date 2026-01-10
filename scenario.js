043ebb9c1a64320284f8327aa5a37145010c606d
Ymtkh21020289 <912112018@gse.okayama-c.ed.jp>
2026-01-11 00:47:02 +0900

    Update scenario.js

--- scenario.js
+++ scenario.js
@@ -25,13 +25,16 @@ export const scenario = {
       { name: null, text: "そう言われると一人の女の子がステージ上に上がった。" },
       //{ name: null, text: "銀色の艶やかな髪で身長は少し低め。" },
       //{ name: null, text: "首にかけているのは…………………………ヘッドホン！？" },
+      { command: "show", chara: "captain", face: "serious", pos:"center" },
       { name: null, text: "彼女はマイクの前に立つと深呼吸をし、口を開いた。" },
+      { command: "face", chara: "captain", face: "serious2" },
       { name: "？？？", text: "「諸君！！この世界はバグで溢れている！！」" },
       { name: null, text: "・・・・・・・・" },
       { name: null, text: "・・・・・・は？" },
       { name: "？？？", text: "「君たちの中にも気づいてる人はいるはずだ！」" },
       { name: "？？？", text: "「このままでは安全な暮らしすらも危うくなってしまう！！」" },
       { name: "？？？", text: "「さぁ！共に立ち上がり、巨悪を打ち倒そうではないか！」" },
+      { command: "hide", chara: "captain" },
       { name: null, text: "そういうと彼女は深々と礼をしてステージを降りた。" },
       { name: null, text: "そういうと彼女は深々と礼をしてステージを降りた。" }
     ]
